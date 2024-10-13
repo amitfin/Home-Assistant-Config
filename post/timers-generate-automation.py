1fde88104bfe2148d780326ecd9139d2577b9a44
Amit Finkelstein <amittein@gmail.com>
2024-10-13 10:27:08 +0300

    Update timers-generate-automation.py

--- post/timers-generate-automation.py
+++ post/timers-generate-automation.py
@@ -1,54 +1,108 @@
 #!/usr/bin/env python3
 import yaml
 
-CONFIG_FILE = 'timers-config.yaml'
-GENERATED_FILE = 'automations/timer-automations.yaml'
-DOMAIN_CONFIG = {
+CONFIG_FILE = '/config/timers/config.yaml'
+GENERATED_FILE = '/config/automations/timer-automations.yaml'
+SERVICE = {
   'switch': {
-    'on': 'turn_on',
-    'off': 'turn_off',
+    'on': 'switch.turn_on',
+    'off': 'switch.turn_off',
   },
   'light': {
-    'on': 'turn_on',
-    'off': 'turn_off',
+    'on': 'light.turn_on',
+    'off': 'light.turn_off',
   },
   'cover': {
-    'on': 'open_cover',
-    'off': 'close_cover',
+    'on': 'cover.open_cover',
+    'off': 'cover.close_cover',
   },
   'climate': {
-    'on': 'turn_on',
-    'off': 'turn_off',
+    'on': 'climate.turn_on',
+    'off': 'climate.turn_off',
+  },
+}
+EXPECTED_STATE = {
+  'switch': {
+    'on': '"on"',
+    'off': '"off"',
+  },
+  'light': {
+    'on': '"on"',
+    'off': '"off"',
+  },
+  'cover': {
+    'on': ['opening', 'open'],
+    'off': ['closing', 'closed'],
+  },
+  'climate': {
+    'on': ['cool', 'heat'],
+    'off': '"off"',
   },
 }
 
-def main():
-  line('# GENERATED FILE - DO NOT EDIT MANUALLY\n')
-  with open(CONFIG_FILE, 'r') as config_file:
-    config = yaml.load(config_file, Loader=yaml.SafeLoader)
-  for rule in config:
-    create(rule)
+class GenerateRules():
+  def __init__(self) -> None:
+    with open(CONFIG_FILE, 'r') as config_file:
+      self._config = yaml.load(config_file, Loader=yaml.SafeLoader)
+    self._output = open(GENERATED_FILE, 'w')
+    self._write('# GENERATED FILE - DO NOT EDIT MANUALLY\n')
 
-def create(config):
-  domain_config = DOMAIN_CONFIG[config['domain']]
-  line('- id: %s'% config['timer'])
-  line('  alias: %s'% config['timer'])
-  line('  trigger:')
-  line('    - platform: state')
-  line('      entity_id: %s, input_timetable.%s' % (
-    config['switch'], config['timer']))
-  line('  condition: "{{ is_state(\'%s\', \'on\') }}"' % config['switch'])
-  line('  action:')
-  line('    - service: %s.{{ iif(is_state(\'input_timetable.%s\', '
-    '\'on\'), \'%s\', \'%s\') }}' % (config['domain'], config['timer'],
-    domain_config['on'], domain_config['off']))
-  line('      entity_id: %s' % ', '.join(map(
-    lambda entity: '%s.%s' % (config['domain'], entity), config['entities'])))
-  line('')
+  def __del__(self) -> None:
+    if self._output:
+      self._output.close()
 
-def line(content):
-  out.write(content + '\n')
+  def __enter__(self):
+    return self
+
+  def __exit__(self, *args):
+    self._output.close()
+    self._output = None
+
+  def _write(self, line: str):
+    self._output.write(line + '\n')
+
+  def process(self):
+    for entry in self._config:
+      self._create_rule(entry)
+
+  def _create_rule(self, config: dict[str, any]):
+    name = config['control'][0].split('.')[1]
+    if not self._unique(config['control'][0]):
+      name += '_' + config['domain']
+    self._write('- id: %s'% name)
+    self._write('  alias: %s'% name.replace("_", " ").title() + " (Generated)")
+    self._write('  mode: restart')
+    self._write('  triggers:')
+    self._write('    - trigger: state')
+    self._write('      entity_id:')
+    for state in config['control']:
+      self._write('        - %s' % state)
+    if 'condition' in config:
+      self._write('        - %s' % config['condition'])
+    self._write('      to: ["on", "off"]')
+    self._write('  actions:')
+    if 'condition' in config:
+      self._write('    - condition: template')
+      self._write('      teamplate_value: "{{ is_state(\'%s\', \'on\') }}"' % config['condition'])
+    self._write('    - if:')
+    self._write('        - condition: template')
+    self._write('          teamplate_value: "{{ %s }}"' % (
+      ' and '.join(map(lambda state: "is_state('%s', 'on')" % state, config['control']))))
+    for state in ['on', 'off']:
+      self._write('      %s:' % ('then' if state == 'on' else 'else'))
+      self._write('        - action: retry.action')
+      self._write('          data:')
+      self._write('            action: %s' % SERVICE[config['domain']][state])
+      self._write('            entity_id:')
+      for entity in config['entities']:
+        self._write('              - %s' % entity)
+      if config['domain'] in EXPECTED_STATE:
+        self._write('            expected_state: %s' % EXPECTED_STATE[config['domain']][state])
+    self._write('')
+
+  def _unique(self, state: str) -> bool:
+    return list(map(lambda x: x['control'][0], self._config)).count(state) == 1
 
 if __name__ == "__main__":
-  with open(GENERATED_FILE, 'w') as out:
-    main()
+  with GenerateRules() as engine:
+    engine.process()
